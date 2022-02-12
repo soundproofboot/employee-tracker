87f39afd7627c0250c97869b996e5ad3dc52bf23
C Bares <colinedwinbares@gmail.com>
2022-02-12 16:45:45 -0600

    just added LEFT for joins to include employees without manager

--- queries/queries.js
+++ queries/queries.js
@@ -3,9 +3,9 @@ const connection = require('./connection')
 const sqlQueries = {
   employeeText: `SELECT e.first_name AS first_name, e.last_name AS last_name, title, salary, name AS department, m.first_name AS manager_first, m.last_name AS manager_last
                 FROM employee AS e
-                JOIN role ON e.role_id = role.id 
-                JOIN department ON role.department_id = department.id
-                JOIN employee AS m ON e.manager_id = m.id
+                LEFT JOIN role ON e.role_id = role.id 
+                LEFT JOIN department ON role.department_id = department.id
+                LEFT JOIN employee AS m ON e.manager_id = m.id
                 `,
   roleText: "SELECT * FROM role",
   departmentText: "SELECT * FROM department",
