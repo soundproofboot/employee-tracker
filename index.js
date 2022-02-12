b867d08ea1e455c4e42ef58366315d05045cbebe
C Bares <colinedwinbares@gmail.com>
2022-02-12 16:43:06 -0600

    setting up join to get all employee info

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
 const menu = require('./questions/menu');
 const cTable = require("console.table");
+const sqlQueries = require('./queries/queries');
 
-async function doTheThing() {
-  let answer = await inquirer.prompt(menu);
-  if (answer.menu === 'Add Employee') {
-    
-  }
-};
-
-
-
-const connection = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "soundproofboot",
-    password: "upicmemberservices",
-    database: "workforce",
-  },
-  console.log("Connected to the workforce database.")
-);
-
-
-
-doTheThing();
+sqlQueries.getEmployees();
+// inquirer.prompt(menu).then(x => console.log(x));
--- queries/queries.js
+++ queries/queries.js
@@ -1,21 +1,32 @@
+const connection = require('./connection')
 
 const sqlQueries = {
-  employeeText: 'SELECT * FROM employee',
-  roleText: 'SELECT * FROM role',
-  departmentText: 'SELECT * FROM department',
-  getEmployees() {
+  employeeText: `SELECT e.first_name AS first_name, e.last_name AS last_name, title, salary, name AS department, m.first_name AS manager_first, m.last_name AS manager_last
+                FROM employee AS e
+                JOIN role ON e.role_id = role.id 
+                JOIN department ON role.department_id = department.id
+                JOIN employee AS m ON e.manager_id = m.id
+                `,
+  roleText: "SELECT * FROM role",
+  departmentText: "SELECT * FROM department",
+  getEmployees: function () {
     connection.query(this.employeeText, (err, results, fields) => {
+      if (err) {
+        console.log(err);
+      }
       console.table(results);
-    })
+    });
   },
-  getRoles() {
+  getRoles: function () {
     connection.query(this.roleText, (err, results, fields) => {
       console.table(results);
-    })
+    });
   },
-  getDepartments() {
+  getDepartments: function () {
     connection.query(this.departmentText, (err, results, fields) => {
       console.table(results);
-    })
-  }
-}
+    });
+  },
+};
+
+module.exports = sqlQueries;
